4809290a241dd7e75ae32cd1b476c04ccec5034a
otiai10 <otiai10@gmail.com>
2014-09-13 22:39:03 +0900

    Add Logger

--- app/src/main/java/com/otiai10/app/alertsample/MyActivity.java
+++ app/src/main/java/com/otiai10/app/alertsample/MyActivity.java
@@ -4,12 +4,14 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.util.Log;
 
 
 public class MyActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("marker00", "MyActivity.onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
     }
@@ -17,6 +19,7 @@ public class MyActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        Log.d("marker00", "MyActivity.onCreateOptionsMenu");
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.my, menu);
         return true;
@@ -24,6 +27,7 @@ public class MyActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        Log.d("marker00", "MyActivity.onOptionsItemSelected");
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
